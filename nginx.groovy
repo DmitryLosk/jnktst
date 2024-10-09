869e89e550db455d68dbda1be7b531fecdded47b
DmitryLosk <dloskut@gmail.com>
2024-10-09 22:12:17 +0300

    test

---
+++ nginx.groovy
@@ -0,0 +1,75 @@
+pipeline {
+    agent {node('agent')}
+
+    environment {
+        IMAGE = 'nginx:latest'
+        C_NAME = 'nginx_test'
+        PORT_MP = '9889:80'
+        EMAIL_RECIPIENT = 'dts2000@ya.ru'
+    }
+
+    stages {
+        stage('Clone Repository') {
+            steps {
+                cleanWs()
+                checkout scm
+            }
+        }
+
+        stage('Run Nginx Container') {
+            steps {
+                script {
+                    sh "docker run -d --name ${C_NAME} -p ${PORT_MP} -v \$(pwd)/index.html:/usr/share/nginx/html/index.html ${IMAGE}"
+                }
+            }
+        }
+
+        stage('Check HTTP Response') {
+            steps {
+                script {
+                    def response = sh(script: "curl -o /dev/null -s -w '%{http_code}' http://localhost:9889", returnStdout: true).trim()
+                    if (response != '200') {
+                        error "HTTP response code is not 200, but ${response}"
+                    }
+                }
+            }
+        }
+
+        stage('Check MD5 Sum') {
+            steps {
+                script {
+                    def local_md5 = sh(script: "md5sum index.html | awk '{ print \$1 }'", returnStdout: true).trim()
+                    def container_md5 = sh(script: "curl -s http://localhost:9889/index.html | md5sum | awk '{ print \$1 }'", returnStdout: true).trim()
+                    if (local_md5 != container_md5) {
+                        error "MD5 sums do not match! Local: ${local_md5}, Container: ${container_md5}"
+                    }
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            script {
+                sh "docker stop ${C_NAME} || true"
+                sh "docker rm -f ${C_NAME} || true"
+            }
+        }
+        success {
+            echo 'Pipeline completed successfully.'
+            script {
+                sh """
+echo "Subject: Jenkins Build Successful: ${currentBuild.fullDisplayName}\n\nGood news! The Jenkins build ${currentBuild.fullDisplayName} completed successfully.\n\nCheck the details at: ${env.BUILD_URL}" | sendmail ${EMAIL_RECIPIENT}
+"""
+            }
+        }
+        failure {
+            echo 'Pipeline failed.'
+            script {
+                sh """
+echo "Subject: Jenkins Build Failed: ${currentBuild.fullDisplayName}\n\nUnfortunately, the Jenkins build ${currentBuild.fullDisplayName} failed.\n\nCheck the details at: ${env.BUILD_URL}" | sendmail ${EMAIL_RECIPIENT}
+"""
+            }
+        }
+    }
+}
